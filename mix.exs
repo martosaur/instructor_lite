56c420f38013ae04b6b70bf8d10ec176df5cbc03
martosaur <martosaur@gmail.com>
2024-09-02 11:22:25 -0700

    Fix main readme

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule InstructorLite.MixProject do
 
   defp docs do
     [
-      main: "README",
+      main: "readme",
       source_url: @source_url,
       source_ref: "v#{@version}",
       extras: [
